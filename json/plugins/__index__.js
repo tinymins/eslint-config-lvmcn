f6990908b4cbdef693d0c6a17598158315a85699
Emil Zhai <root@derzh.com>
2022-02-24 17:18:27 +0800

    feat: add json lint configures

---
+++ json/plugins/__index__.js
@@ -0,0 +1,9 @@
+/**
+ * This file is part of eslint-config-lvmcn
+ * @link     : https://zhaiyiming.com/
+ * @author   : Emil Zhai (root@derzh.com)
+ * @modifier : Emil Zhai (root@derzh.com)
+ * @copyright: Copyright (c) 2018 TINYMINS.
+ */
+
+module.exports.base = require('./base.js');
